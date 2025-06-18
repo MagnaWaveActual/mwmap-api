1cebcc59e514e1802cf406206537a36b3dfd3a73
David Allen the Second <softwaredevelopment@magnawavepemf.com>
2025-06-18 10:06:59 -0400

    Dev New Updates from Equinox Team
    Dev New Updates from Equinox Team, will document total of changes via the pull into dev
    
    Co-Authored-By: D2 <11322006+votlon@users.noreply.github.com>

--- app/mailers/location_mailer.rb
+++ app/mailers/location_mailer.rb
@@ -1,38 +1,31 @@
 class LocationMailer < ApplicationMailer
+    def lead_for_all_email(email, s_name, s_phone, s_email, message)  
+      @s_name = s_name
+      @s_email = s_email
+      @s_phone = s_phone
+      @s_message = message
 
-    def lead_for_all_email(email, s_name, s_phone, s_email, message)
-      
-        @s_name = s_name
-        @s_email = s_email
-        @s_phone = s_phone
-        @s_message = message
+      p @s_phone
+      p s_phone
+      mail(to: email, subject: "New Map Lead")
+    end
+  
+    def lead_for_one_email(email, business_name, s_name, s_phone, s_email, message)
+      @s_name = s_name
+      @s_email = s_email
+      @s_phone = s_phone
+      @b_name = business_name
+      @s_message = message
+      mail(to: email, subject: "New Exclusive Map Lead")
+    end
 
-        p @s_phone
-        p s_phone
-        mail(to: email, subject: "New Map Lead")
-  
-      end
-  
-  
-      def lead_for_one_email(email, business_name, s_name, s_phone, s_email, message)
-     
-        @s_name = s_name
-        @s_email = s_email
-        @s_phone = s_phone
-        @b_name = business_name
-        @s_message = message
-        mail(to: email, subject: "New Exclusive Map Lead")
-  
-      end
+  # === MODIFIED SECTION START ===
+  # === Modified by Adnan at EquinoxAgents June 12, 2025
+  # MOFIFIED: def test_email — modified test method for testing with custom email and password on 2025-06-14 10:54:09
 
-      def test_email()
-       
-        delivery_options = { user_name: "leads@magnawaveportal.com",
-                         password: "MagnaWave1!2024",
-                         address: "smtp.mailgun.org",
-                        port: 587,
-                      domain: "magnawaveportal.com"}
-        mail(to: "alex@magnawavepemf.com", subject: "Test MW Email")
-      end
+  def test_email(to, subject)
+    mail(to: to, subject: subject, body: "This is a test email from MW.")
+  end
+  # === MODIFIED SECTION END ===
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,33 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  
+  # === MODIFIED SECTION START ===
+  # === Modified by Adnan at EquinoxAgents June 12, 2025
+  
+  # Mailer settings
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default_options = { from: "leads@magnawaveportal.com" }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              ENV['SMTP_ADDRESS'],
+    port:                 587,
+    domain:               ENV['SMTP_DOMAIN'],
+    user_name:            ENV['SMTP_USERNAME'], 
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
+  # === Suggest adding env variables for the username/pw above for security
+  # === MODIFIED SECTION END ===
+
+
+
 end
