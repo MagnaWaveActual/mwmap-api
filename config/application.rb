c30ffbfc5669b16f45b587ffc828b1ac936a9ec4
Rahul <rahul97811@gmail.com>
2025-07-02 12:32:30 +0530

    Ensure job and mailer classes load correctly in API-only app

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module AuthBaseApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.eager_load_paths += %W(#{config.root}/app/jobs #{config.root}/app/mailers)
+    config.active_job.queue_adapter = :sidekiq
   end
 end
